febbf8263e44d25ceaefd7faeb4f2f6e454e5e49
patrick <786156072@qq.com>
2024-03-31 18:09:59 +0800

    fix: some issues

--- pages/profile/[id]/index.vue
+++ pages/profile/[id]/index.vue
@@ -3,11 +3,17 @@ definePageMeta({
   layout: 'profile-layout',
 })
 
-const { data } = await useAsyncData('getPosts', () => {
+const { data, status } = await useAsyncData('getPosts', () => {
   return useGetSelfPosts()
 })
 </script>
 
 <template>
-  <GridPostList :posts="data || []" :show-user="false" />
+  <div v-if="status === 'pending'" class="flex w-full items-center justify-center">
+    <Loader />
+  </div>
+  <p v-else-if="!data || data.length === 0" class="text-primary/60">
+    No posts
+  </p>
+  <GridPostList v-else :posts="data || []" :show-user="false" />
 </template>
